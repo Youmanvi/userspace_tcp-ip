928e8310142408877c8b1df33f580236e7da0067
Youmanvi <vihan.yerubandi@gmail.com>
2026-01-23 08:42:41 -0600

    implementing base protocol

---
+++ circle_buffer.hpp
@@ -0,0 +1,37 @@
+
+#pragma once
+#include <memory>
+#include <optional>
+#include <queue>
+namespace uStack {
+
+    template <typename PacketType>
+    class circle_buffer {
+    private:
+            std::queue<PacketType> packets;
+
+    public:
+            bool
+            empty() {
+                    return packets.size() == 0;
+            }
+            void
+            push_back(PacketType packet) {
+                    packets.push(std::move(packet));
+            }
+            
+            int 
+            size(){
+                    return packets.size();
+            }
+            std::optional<PacketType>
+            pop_front() {
+                    if (empty()) {
+                            return std::nullopt;
+                    }
+                    auto packet = std::move(packets.front());
+                    packets.pop();
+                    return std::move(packet);
+            }
+    };
+};  // namespace uStack
